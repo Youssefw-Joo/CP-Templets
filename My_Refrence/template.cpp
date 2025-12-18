10ce4adbd01d5297b2960beef7dfb171ed6b0c00
Youssef Waleed Hussein <yousefabdelraouf@std.mans.edu.eg>
2025-12-18 04:23:10 +0200

    Update template.cpp

--- My_Refrence/template.cpp
+++ My_Refrence/template.cpp
@@ -1,16 +1,22 @@
+// Author : JOOOE
 #include <bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 using namespace std;
 using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet;
 #define Goat ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-#define int long long
 #define endl '\n'
-#define ld long double
+#define ld  long double
+#define all(a)       a.begin(), a.end()
+#define allr(a)      a.rbegin(), a.rend()
+#define Max(a)      *max_element(all(a))
+#define Min(a)      *min_element(all(a))
+#define yes  cout << "YES" << endl
+#define no   cout << "NO" << endl
+const int mod = 1e9+7;
 
 void test()
 {
-
 }
 
 signed main()
@@ -18,4 +24,5 @@ signed main()
     Goat
     int t = 1; //cin >> t;
     while (t--) test();
+    return 0;
 }
